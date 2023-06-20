496c2d239382aa93fe75e791141d8a8b345f89cf
tobiaspoehnl <tobias1.poehnl@st.oth-regensburg.de>
2023-06-20 15:34:14 +0200

    svelte basic structure

---
+++ placemark-svelte/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Header from '$lib/Header.svelte';
+</script>
+
+<Header />
+
+<div class="columns is-vcentered content">
+  <div class="column has-text-centered">
+    <img width="300" src="/homer.png" alt="homer" />
+  </div>
+  <div class="column">
+    <h1 class="title">Help Me Run Springfield</h1>
+    <p>Donate what you can now - No Bitcoins accepted!</p>
+  </div>
+</div>
