8228a20c01b30add7545a6a7d943a405f38ac316
tobiaspoehnl <tobias1.poehnl@st.oth-regensburg.de>
2023-06-30 19:33:45 +0200

    eight fix attempt signup svelte

--- hapi/src/api/user-api.js
+++ hapi/src/api/user-api.js
@@ -43,7 +43,7 @@ export const userApi = {
     },
 
     create: {
-        auth: {strategy: false},
+        auth: false,
         handler: async function (request, h) {
             try {
                 const user = await db.userStore.addUser(request.payload);
