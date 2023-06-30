e990986d0c43e63034f851ecc9f6f604810ba896
tobiaspoehnl <tobias1.poehnl@st.oth-regensburg.de>
2023-06-30 23:08:57 +0200

    final fix edit placemark svelete

--- hapi/src/api/placemark-api.js
+++ hapi/src/api/placemark-api.js
@@ -120,10 +120,10 @@ export const placemarkApi = {
             try {
                 const loggedInUser = request.auth.credentials;
                 const updatedPlacemark = request.payload
-                if(!loggedInUser.adminrights && !loggedInUser._id.equals(updatedPlacemark.createdby)){
+                const placemark = await db.placemarkStore.getPlacemarkById(request.params.id)
+                if(!loggedInUser.adminrights && !loggedInUser._id.equals(placemark.createdby)){
                     return true;
                 }
-                const placemark = await db.placemarkStore.getPlacemarkById(request.params.id)
                 await db.placemarkStore.updatePlacemark(placemark, updatedPlacemark)
                 return true;
             } catch (err) {
